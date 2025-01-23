afa7415dbb4c3b5c81b3c0c760044baf95b95712
KEVIN STEVEN MONROY IBARRA <KEVINSMONR@protela.local>
2025-01-23 10:49:46 -0500

    Crud con sus funciones principales y basicas compleyadas

--- crud.py
+++ crud.py
@@ -4,23 +4,19 @@ deportes = ""
 estrato = ""
 ##################################
 def deportess():
-    print (" si o no")
-    deporte = str(input("Juega algun deporte :")).lower()
-    print (" si o no")
-    deporte = str(input("Juega algun deporte :")).lower()
+    print (" Responda esta pregunta con un si o no")
+    deporte = str(input("Juega algun deporte:")).lower()
     if deporte  == 'si':
         deportes = (str(input("Digite el deporte que juega el niño :")))
     elif deporte != 'si' or deporte != 'no':
         print ("escoja una opcion valida")
         deportess()
-        deportes = (str(input("Digite el deporte que juega el niño :")))
-    elif deporte != 'si' or deporte != 'no':
-        print ("escoja una opcion valida")
-        deportess()
+    return deportes
 
 
 def estratos():
     print("Los tipos de estratos a escoger son: ")
+    print("Responda la pregunta segun estas opciones")
     print("Alto - Medio - Bajo ")
     estrato = str(input("Digite el estrato del niño : ")).lower()
     if estrato == 'alto' or 'medio' or 'bajo':
@@ -28,32 +24,19 @@ def estratos():
     elif estrato != 'alto' or estrato != 'medio' or estrato != 'bajo':   
         print("Digite el estrato de manera correcta")
         estratos()
+    return estrato
 
 
 def crear():
     tarjeta_iden = int(input("Digite la tarjeta de identidad del niño "))
     nombre = str(input("Digite el nombre del niño: "))
     apellido = str(input("Digite el apellido del niño: "))
-    estrato = str(input("Digite el estrato del niño : ")).lower()
-    if estrato == 'alto' or 'medio' or 'bajo':
-        print(estrato)
-    elif estrato != 'alto' or estrato != 'medio' or estrato != 'bajo':   
-        print("Digite el estrato de manera correcta")
-        estratos()
-
-
-def crear():
-    tarjeta_iden = int(input("Digite la tarjeta de identidad del niño "))
-    nombre = str(input("Digite el nombre del niño: "))
-    apellido = str(input("Digite el apellido del niño: "))
-    estratos()
-    deportess()
-    edad = int(input("Digite la edad del niño: "))
-    genero = str(input("Digite el genero del niño: "))
+    estrato = estratos()
+    deportes = deportess()
     edad = int(input("Digite la edad del niño: "))
     genero = str(input("Digite el genero del niño: "))
 
-    nino[tarjeta_iden] = {  
+    ninos.append({  
             'tarjeta_identidad': tarjeta_iden,
             'nombre': nombre,
             'apellido': apellido,
@@ -61,12 +44,47 @@ def crear():
             'deporte': deportes,
             'edad': edad,
             'genero': genero
-        }
+        })
 
 def leer():
-    ninos.append({nino})
+    if len(ninos) == 0:  
+        print("No hay niños registrados.")
+    else:
+        for n in ninos:  
+            print("\n Información del Niño ")
+            print("Tarjeta de Identidad:", n['tarjeta_identidad'])
+            print("Nombre:", n['nombre'])
+            print("Apellido:", n['apellido'])
+            print("Edad:", n['edad'])
+            print("Estrato:", n['estrato'])
+            print("Deporte:", n['deporte'])
+            print("Genero:", n['genero'])
+            print("\n")
+
+
+def actualizar():
+    tarjeta_iden = int(input("Digite la tarjeta del niño que desea modificar "))
     for n in ninos:
-        print(ninos.values())
+        if n ['tarjeta_identidad'] == tarjeta_iden:
+            print("Niño encontrado ahora modifique la informacion")
+            n ["Nombre"] = str(input("Digite de nuevo el nombre: "))
+            n ["Apellido"] = str(input("Digite de nuevo apellido: "))
+            n ["Edad"] = int(input("Digite de nuevo la edad: "))
+            n ["Estrato"] =  estratos()
+            n ["Deporte"] =  deportess()
+            n ["Genero"] = str(input("Digite de nuevo el genero: "))
+            print("Niño modificado")
+        else :
+            print("No se encontro niño")
+
+def eliminar():
+    tarjeta_iden = int(input("Digite la tarjeta de identidad del niño que desea eliminar: "))
+    for n in ninos:
+        if n['tarjeta_identidad'] == tarjeta_iden:
+            ninos.remove(n)
+            print(f"Niño con tarjeta de identidad {tarjeta_iden} eliminado exitosamente.")
+        else:
+            print("No se encontró un niño con esa tarjeta de identidad.")
 
 #Menu#
 
@@ -74,23 +92,22 @@ while True:
     print ("--------------------------------------------")
     print ("Menu:")
     print ("Escoja de manera correcta")
-    print ("Escoja de manera correcta")
     print ("1. Registrar Niño")
     print ("2. Listar Niños")
-    print ("3. Salir")
+    print ("3. Actualizar Niño")
+    print ("4. Eliminar Niño")
+    print ("5. Salir")
     opcion = int(input("Escoja una opcion: "))
     if opcion == 1:
         crear()
-    if opcion == 2:
+    elif opcion == 2:
         leer()
-    if opcion == 3:
+    elif opcion == 3:
+        actualizar()
+    elif opcion == 4:
+        eliminar()
+    elif opcion == 5:
+        print("Fin del programa")
         break
-
-
-
-
-
-
-
-
-
+    else:
+        print("Opción no válida. Por favor, seleccione una opción correcta.")
