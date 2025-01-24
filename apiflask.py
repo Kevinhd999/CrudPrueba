2ed3b8380c3900fb745f548cfbc420fe8f48038a
KEVIN STEVEN MONROY IBARRA <KEVINSMONR@protela.local>
2025-01-24 09:47:15 -0500

    Redireccionamiento direccions

--- apiflask.py
+++ apiflask.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
+@app.route('/home', methods = ['GET'])
 
 def hola_mundo():
-    return "Hola mundo"
+    user_ip_information = request.remote_addr # request:remote.addr detecta el ip del cliente que hace la solicitud 
+    response = make_response(redirect("/Redireccion")) 
+    response = set.cookies ("user_ip_information", user_ip_information)
+    return response
+
+@app.route('/Redireccion')
+def informacion():
+    user_ip = request.cookies.get("user_ip_information")
+    return f"Su ip es {user_ip}"
 
 if __name__ == '__main__':
-    app.run(port = 3000)
+    app.run(port = 3000, debug = True)
