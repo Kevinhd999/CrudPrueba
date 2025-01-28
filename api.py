7a4a9e52f59ed573eb28d87819e9034b632488c0
KEVIN STEVEN MONROY IBARRA <KEVINSMONR@protela.local>
2025-01-28 10:44:40 -0500

    Crud con api y su respectiva base de datos funcional

--- api.py
+++ api.py
@@ -1,11 +1,5 @@
-from flask import Flask, request, make_response, redirect, abort, jsonify
-#flask es un micro framework, y markupsafe es una biblioteca de flask
-#Make_response se encarga de cambiar el tipo de peticion
-#Redirect se encarga de dirigir la nueva url 
-#jsonify se encarga de convertir los datos en formato json
-#abort se encarga de cerrar la vista para autorizacion denegada 
-
-datos = []
+from flask import Flask, request, abort, jsonify
+from crud import crear_usuario, lista_usuarios, usuario_por_cedula, actualizar_usuario, eliminar_usuario
 
 app = Flask(__name__)
 
@@ -13,76 +7,72 @@ app = Flask(__name__)
 def incial():
     return jsonify({"message": "Hola acabas de entrar a esta pagina"})
 
-
-@app.route("/usuario", methods = ['GET'])
-def usuarios():
-    if datos == []:
-        return jsonify({"message": "No hay usuarios registrados"}), 204
+@app.route("/usuario", methods=['GET'])
+def listar_usuarios():
+    usuarios = lista_usuarios()
+    if not usuarios:
+        return '', 204  # No hay usuarios registrados
     else:
-         return jsonify({"message": "Lista de usuarios", "data": datos})
+        return jsonify(usuarios), 200
 
 @app.route('/registrar_usuario', methods=['POST'])
-def anadir_dato():
-        if not request.json or not 'cedula' in request.json or not 'nombre' in request.json or not 'edad' in request.json:
-            #if not request.json se encarga que los datos respectivos sean enviados en formato json
-            abort(400)  
-        for usuario in datos:
-            if usuario['cedula'] == int(request.json['cedula']):
-                return jsonify({"mensaje": "La cedula ya esta registrada", "data": usuario}), 400
-        nuevo_dato = {
-            "cedula": int(request.json['cedula']),
-            "nombre": request.json['nombre'],
-            "edad": int(request.json['edad'])
-        }
-        datos.append(nuevo_dato)
-        return jsonify({"mensaje": "Datos agregados correctamente", "data": nuevo_dato}), 201
+def registrar_dato():
+    if not request.json or not 'cedula' in request.json or not 'nombre' in request.json or not 'edad' in request.json:
+        abort(400)
+    cedula = request.json['cedula']
+    nombre = request.json['nombre']
+    edad = request.json['edad']
 
+    if usuario_por_cedula(cedula):
+        return jsonify({"mensaje": "La cédula ya está registrada"}), 400
+
+    crear_usuario(cedula, nombre, edad)
+    return jsonify({"mensaje": "Datos agregados correctamente"}), 201
 
 @app.route('/editar_usuario/<int:cedula_id>', methods=['PUT'])
 def editarusuario(cedula_id):
-    usuario = next((usuario for usuario in datos if usuario['cedula'] == cedula_id), None)
-    if usuario is None:
-        abort(404)
-    elif not request.json:
-        abort(400)
-    
-    usuario['nombre'] = request.json.get('nombre', usuario['nombre'])
-    usuario['edad'] = request.json.get('edad', usuario['edad'])
-    
-    return jsonify({"message": "Usuario actualizado", "data": usuario})
-
-@app.route('/editar_key_usuario/<int:cedula_id>', methods=['PATCH'])
-def actualizar_usuario_parcial(cedula_id):
-    usuario = next((usuario for usuario in datos if usuario['cedula'] == cedula_id), None)
+    usuario = usuario_por_cedula(cedula_id)
     if usuario is None:
         abort(404)
     if not request.json:
         abort(400)
+    nombre = request.json.get('nombre', usuario[1]) 
+    edad = request.json.get('edad', usuario[2])  
     
-    # Actualizar solo los campos proporcionados en la solicitud
+
+    actualizar_usuario(cedula_id, nombre, edad)
+    return jsonify({"message": "Usuario actualizado", "data": {"cedula": cedula_id, "nombre": nombre, "edad": edad}}), 200
+
+@app.route('/editar_key_usuario/<int:cedula_id>', methods=['PATCH'])
+def actualizar_usuario_parcial(cedula_id):
+    usuario = usuario_por_cedula(cedula_id)
+    if usuario is None:
+        abort(404)
+    if not request.json:
+        abort(400)
+        
+    nombre = usuario[1]
+    edad = usuario[2] 
+
     if 'nombre' in request.json:
-        usuario['nombre'] = request.json['nombre']
+        nombre = request.json['nombre']
     if 'edad' in request.json:
-        usuario['edad'] = int(request.json['edad'])
-    
-    return jsonify({"mensaje": "Usuario actualizado parcialmente", "data": usuario})
+        edad = int(request.json['edad'])
+
+    actualizar_usuario(cedula_id, nombre, edad)
+    return jsonify({"mensaje": "Usuario actualizado parcialmente"}), 200
 
 @app.route('/eliminar_usuario/<int:cedula_id>', methods=['DELETE'])
 def eliminarusuario(cedula_id):
-    usuario = next((usuario for usuario in datos if usuario['cedula'] == cedula_id), None)
+    usuario = usuario_por_cedula(cedula_id)
     if usuario is None:
         abort(404)
-    datos.remove(usuario)
-    return jsonify({"message": "Usuario eliminado", "data": datos})
-
-
-
+    eliminar_usuario(cedula_id)
+    return jsonify({"message": "Usuario eliminado"}), 200
 
 @app.errorhandler(404)
-def error_endpoint(error): #el parametro de error es de la funcion error_endpoint
+def error_endpoint(error):
     return f"Peticion no encontrada{error}", 404
 
-
-#----------------------------------------------------------------------------------#
 if __name__ == '__main__':
-    app.run(port = 5000, host='0.0.0.0', debug = True)
+    app.run(port=5000, host='0.0.0.0', debug=True)
