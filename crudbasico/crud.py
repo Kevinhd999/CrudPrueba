b136890067384ec9f9de22aeea488542f09e3b3e
KEVIN STEVEN MONROY IBARRA <KEVINSMONR@protela.local>
2025-01-29 09:34:50 -0500

    El codigo principal antes de la refactorizacion

---
+++ crudbasico/crud.py
@@ -0,0 +1,44 @@
+# from model.db_manager import DATABASE_URL 
+from conexion import db_connection
+
+def crear_usuario(cedula, nombre, edad):
+    connection =  db_connection()
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO usuarios (cedula, nombre, edad) VALUES (%s, %s, %s)", (cedula, nombre, edad))
+    connection.commit()# Confirma el movimiento o la transacción
+    cursor.close()
+    connection.close()
+
+def lista_usuarios():
+    connection =  db_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM usuarios")
+    usuarios = cursor.fetchall()# Devuelve todos los usuarios 
+    cursor.close()
+    connection.close()
+    return usuarios
+
+def usuario_por_cedula(cedula):
+    connection =  db_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM usuarios WHERE cedula = %s", (cedula,))
+    usuario = cursor.fetchone()# Devuelve un solo usuario si lo encuentra
+    cursor.close()
+    connection.close()
+    return usuario
+
+def actualizar_usuario(cedula, nombre, edad):
+    connection =  db_connection()
+    cursor = connection.cursor()
+    cursor.execute("UPDATE usuarios SET nombre = %s, edad = %s WHERE cedula = %s", (nombre, edad, cedula))
+    connection.commit() # Confirma el movimiento o la transacción
+    cursor.close()
+    connection.close()
+
+def eliminar_usuario(cedula):
+    connection = db_connection()
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM usuarios WHERE cedula = %s", (cedula,))
+    connection.commit()# Confirma el movimiento o la transacción
+    cursor.close()
+    connection.close()
---
+++ crudbasico/experimentosflask.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, make_response, redirect, abort
+
+#flask es un micro framework, y markupsafe es una biblioteca de flask
+#Make_response se encarga de cambiar el tipo de peticion
+#Redirect se encarga de dirigir la nueva url 
+
+app = Flask(__name__)
+
+@app.route('/')
+def hola_mundo():
+    return "Hola acabas de entrar a esta pagina"
+
+@app.errorhandler(404)
+def error_endpoint(error): #el parametro de error es de la funcion error_endpoint
+    return f"Peticion no encontrada{error}", 404
+
+@app.route('/usuario/<username>')
+def perfil_usuruaio(username): #es un parametro de ruta, para poder asisgnar la variable 
+    username = "Martin"
+    return f"usuario {username}"
+
+#@app.route('/ciclo')
+#def index():
+#    return redirect(url_for('/home'))
+
+@app.route('/prohibido', methods = ['GET'])
+def login():
+    abort(401) #abort genera que se cierre la vista, lo que puede indicar que el usuario no tiene permisos para ingresar a esa url
+
+
+@app.route("/home", methods = ['GET'])
+
+def redireccion():
+    user_ip_information = request.remote_addr # request.remote.addr detecta el ip del cliente que hace la solicitud 
+    response = make_response(redirect("/redireccion")) #La manera de crear la nueva direccion y redirect lo dirige 
+    response.set_cookie("user_ip_information", user_ip_information)#Almacena las cookies del cliente
+    return response
+
+
+@app.route("/redireccion", methods = ['GET'])
+def informacion():
+    user_ip = request.cookies.get("user_ip_information") #request.cookies.get solicita las cookies 
+    return f"Su ip es {user_ip}" #retorna la varible user_ip 
+
+
+#----------------------------------------------------------------------------------#
+if __name__ == '__main__':
+    app.run(port = 5000, host='0.0.0.0', debug = True)
