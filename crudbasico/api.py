07841412263d362a21618a8680947745a6ae63bc
KEVIN STEVEN MONROY IBARRA <KEVINSMONR@protela.local>
2025-01-29 15:33:07 -0500

    Refactorizacion de el crud, solo la script crud.py y api.py

--- crudbasico/api.py
+++ crudbasico/api.py
@@ -4,11 +4,22 @@ from model.db_manager import validate_and_update_tables
 
 app = Flask(__name__)
 
+def validar_numero(cedula, edad):
+    if not (8 <= len(str(cedula)) <= 10):
+        return jsonify({"mensaje": "La cédula debe tener entre 8 y 10 dígitos"}), 400
+    if int(cedula) < 0:
+        return jsonify({"mensaje": "La cédula no puede ser negativa"}), 400
+    if int(edad) < 0:
+        return jsonify({"mensaje": "La edad no puede ser negativa"}), 400
+    if int(edad) > 100:
+        return jsonify({"mensaje": "La edad no puede ser verdad"}), 400
+    return None
+
 # Validar y actualizar las tablas al iniciar la aplicación
 validate_and_update_tables()
 
 @app.route('/')
-def incial():
+def inicial():
     return jsonify({"message": "Hola acabas de entrar a esta pagina"})
 
 @app.route("/usuario", methods=['GET'])
@@ -25,33 +36,28 @@ def registrar_dato():
         cedula = request.json['cedula']
         nombre = request.json['nombre']
         edad = request.json['edad']
-        if not (8 <= len(str(cedula)) <= 10):
-            return jsonify({"mensaje": "La cédula debe tener entre 8 y 10 dígitos"}), 400
-        if int(cedula) < 0:  
-            return jsonify({"mensaje": "La cédula no puede ser negativa"}), 400
-        if int(edad) < 0:
-            return jsonify({"mensaje": "La edad no puede ser negativa"}), 400
-        if int(edad) > 100:
-            return jsonify({"mensaje": "La edad no puede ser verdad"}), 400
+        error_response = validar_numero(cedula, edad)
+        if error_response:
+            return error_response
 
     if usuario_por_cedula(cedula):
-        return jsonify({"mensaje": "La cédula ya está registrada"}), 409 # conflicto en la solicitud
+        return jsonify({"mensaje": "La cédula ya está registrada"}), 409  # Conflicto en la solicitud
 
     crear_usuario(cedula, nombre, edad)
     return jsonify({"mensaje": "Datos agregados correctamente"}), 201
 
 @app.route('/editar_usuario/<int:cedula_id>', methods=['PUT'])
-def editarusuario(cedula_id):
+def editar_usuario(cedula_id):
     if request.json:
         usuario = usuario_por_cedula(cedula_id)
-        nombre = request.json.get('nombre', usuario[1]) 
-        edad = request.json.get('edad', usuario[2])  
         if usuario is None:
             abort(404)
-        if int(edad) < 0:
-                return jsonify({"mensaje": "La edad no puede ser negativa"}), 400
-        if int(edad) > 100:
-                return jsonify({"mensaje": "La edad no puede ser verdad"}), 400
+        nombre = request.json.get('nombre', usuario[1])
+        edad = int(request.json.get('edad', usuario[2]))
+
+        error_response = validar_numero(cedula_id, edad)
+        if error_response:
+            return error_response
 
     actualizar_usuario(cedula_id, nombre, edad)
     return jsonify({"message": "Usuario actualizado", "data": {"cedula": cedula_id, "nombre": nombre, "edad": edad}}), 200
@@ -62,19 +68,22 @@ def actualizar_usuario_parcial(cedula_id):
     if usuario is None:
         abort(404)
 
-    nombre = usuario[1]
-    edad = usuario[2] 
+    nombre = request.json.get('nombre', None)
+    edad = int(request.json.get('edad', 0))
 
     if 'nombre' in request.json:
         nombre = request.json['nombre']
     if 'edad' in request.json:
         edad = int(request.json['edad'])
+        error_response = validar_numero(cedula_id, edad)
+        if error_response:
+            return error_response
 
     actualizar_usuario(cedula_id, nombre, edad)
     return jsonify({"mensaje": "Usuario actualizado parcialmente"}), 200
 
 @app.route('/eliminar_usuario/<int:cedula_id>', methods=['DELETE'])
-def eliminarusuario(cedula_id):
+def borrar_usuario(cedula_id):
     usuario = usuario_por_cedula(cedula_id)
     if usuario is None:
         abort(404)
@@ -83,9 +92,7 @@ def eliminarusuario(cedula_id):
 
 @app.errorhandler(404)
 def error_endpoint(error):
-    return f"Peticion no encontrada{error}", 404
-
-
+    return f"Petición no encontrada: {error}", 404
 
 if __name__ == '__main__':
-    app.run(port=5000, host='0.0.0.0', debug=False)
+    app.run(port=5000, host='0.0.0.0', debug=True)
--- crudbasico/model/db_manager.py
+++ crudbasico/model/db_manager.py
@@ -4,30 +4,39 @@ from sqlalchemy import create_engine, inspect, Column, String, Integer
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+# Cargar las variables de entorno
 load_dotenv()
+
+# Definir la base para las clases de modelos
 Base = declarative_base()
 
+# Crear la URL de la base de datos
 DATABASE_URL = (
     f"mysql+mysqlconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
     f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 )
+
+# Crear el motor de la base de datos
 engine = create_engine(DATABASE_URL)
 
-class infoKids(Base):
-    __tablename__ = 'info_kids'
+# Definir la clase del modelo infoKids
+class InfoKids(Base):
+    __tablename__ = 'prueba'
 
     cedula = Column(Integer, primary_key=True, nullable=False)
     nombre = Column(String(50), nullable=False)
     años = Column(Integer, nullable=False)
 
+# Función para validar y actualizar las tablas
 def validate_and_update_tables():
     inspector = inspect(engine)
 
-    # Validar y crear la tabla 'info_kids'
-    if 'info_kids' not in inspector.get_table_names():
+    # Validar y crear la tabla 'info_kids' si no existe
+    if 'prueba' not in inspector.get_table_names():
         print("Creando tabla 'info_kids'...")
-        infoKids.__table__.create(engine)
+        InfoKids.__table__.create(engine)
 
+# Crear una sesión de la base de datos
 Session = sessionmaker(bind=engine)
 session = Session()
 # Llamar a la función para validar y actualizar las tablas
